201f478b30bb7fecb8f002c3dd156015110e4a92
sagar <dhakalsagar2000@gmail.com>
2021-09-17 20:20:28 +0545

    some new packages and db.js changed

--- db.js
+++ db.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfull!!");
+const connectToMongo = async ()=>{
+    await mongoose.connect(mongoURI, (err, res)=>{
+        try {
+            console.log("Connected to Mongo Successfull!!");
+            
+        } catch (err) {
+            console.log(err);
+        }
 
     })
 }
